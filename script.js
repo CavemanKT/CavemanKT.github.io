f62b0477fbb6e202a2a18e5c01e8fef9733eb0b4
vkoubbeago <vkoubbeago@gmail.com>
2021-07-29 01:02:36 +0800

    created my 1st portfolio

---
+++ script.js
@@ -0,0 +1,45 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+const nav = document.getElementById('nav-bar');
+
+
+let dark = true;
+let light = false;
+
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(dark);
+        // darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(light);
+        // lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+// console.log(currentTheme);
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(dark);
+    } 
+}
