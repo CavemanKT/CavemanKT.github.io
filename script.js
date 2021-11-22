1594a460b058d38cf28e462196116a572a1fe466
CavemanKT <vkoubbeago@gmail.com>
2021-11-22 23:56:13 +0800

    a

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
 
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-    
+
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
 }
 
@@ -34,12 +34,12 @@ toggleSwitch.addEventListener('change', switchTheme);
 
 // Check Local Storage For Theme
 const currentTheme = localStorage.getItem('theme');
-// console.log(currentTheme);
+//
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
         toggleDarkLightMode(dark);
-    } 
+    }
 }
